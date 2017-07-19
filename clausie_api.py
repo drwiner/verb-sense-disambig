fc4e1efd6209aba04a2988c865af20b25bdff694
drwiner <drwiner131@gmail.com>
2017-07-18 20:21:20 -0600

    api

--- clausie_api.py
+++ clausie_api.py
@@ -1,6 +1,7 @@
 from functools import partial
 from nltk.corpus import wordnet as wn
 from subprocess import Popen, PIPE
+from collections import namedtuple
 
 CLAUSIE_FRAME_DICT = {
 	'SVC': [4,6,7],
@@ -12,18 +13,48 @@ CLAUSIE_FRAME_DICT = {
 	'SVOA': [1,2,8,9,10,11,15,16,17,18,19,20,21,30,31,33,24,28,29,32,35]}
 
 
-class ClausieLine:
-	# API for reading from clausie text output
 
-	def __init__(self, sub_lines):
-		self.raw = sub_lines
-		"""
-		# first line is of the form::  # Line x: "sentence"
-		# second line begins semantic graph, indented subsequent lines
-		# third item is of the form:: # Detect x clause(s), subsequent lines start with "-SVC" style
-			SVC style lines, start with hyphen, then parentheses, then dictionary with @ (sometimes) pointing to line
-				# we can try splittling at @ and just take the first part, and if len(split) > 1, last entry [-1]
-		"""
+Clause = namedtuple('Clause', ['type', 'dict'])
+Sentence = namedtuple('ClauseLine', ['raw_sentence', 'clauses', 'triples'])
+
+def to_typed_clause(clause_line):
+	first_part = clause_line.split('(')[0]
+	clause_type = first_part.split('-')[0].strip()
+	clause_parenth = clause_line[len(first_part):].strip()
+	clause_parenth = clause_parenth.replace('(', ' { ')
+	clause_parenth = clause_parenth.replace(',', ' , ')
+	clause_parenth = clause_parenth.replace(')', ' } ')
+	clause_parenth = clause_parenth.replace('@', '_')
+	clause_parenth = clause_parenth.replace(':', ' :')
+	c_list = clause_parenth.split()
+	c_list = ["\'" + c_item + "\'" if c_item not in ['}', '{', ':', ','] else c_item for c_item in c_list]
+	clause_parenth = ' '.join(c_list)
+	clause_dict = eval(clause_parenth)
+	return Clause(clause_type, clause_dict)
+
+def to_clause_line(sub_lines):
+	raw_sentence = sub_lines[0][10:].strip()
+	clauses = []
+	triples = []
+
+	# skip semantic graph?
+	for i, line in enumerate(sub_lines[1:]):
+		if line[4:12] == 'Detected':
+			# find num clauses
+			num_clauses = int(line[13:15].strip())
+			break
+
+	# each subsequent line is a clause
+	clause_lines = [sub_lines[i + 2 + k] for k in range(num_clauses)]
+	for clause in clause_lines:
+		clauses.append(to_typed_clause(clause))
+
+	# skip next line, original sentence (+1)
+	for cndt in sub_lines[i + 2 + num_clauses + 1:]:
+		split_cndt = cndt.split('\t')
+		triples.append(split_cndt[1:4])
+
+	return Sentence(raw_sentence, clauses, triples)
 
 
 def setup_clausie():
@@ -31,9 +62,9 @@ def setup_clausie():
 		['java', '-jar',
 		 'D:/Documents/Python/ClausIEpy/clausie.jar', '-c',
 		 'D:/Documents/Python/ClausIEpy/resources/clausie.conf',
-		 '-v', '-s' '-p', '-o', 'clausie_output.txt'],
+		 '-v', '-s', '-p', '-o', 'clausie_output.txt'],
 		stdin=PIPE)
-	return partial(clausie, clausie_port)
+	return clausie_port
 
 
 def clausie(input_parser=None, text=None, restart=None):
@@ -44,48 +75,10 @@ def clausie(input_parser=None, text=None, restart=None):
 
 	for sent in text:
 		parser.stdin.write(sent)
+
 	parser.communicate()
-
-	# TODO: improve API here.
-	clause_types = []
-	verb_lemmas = []
-	with open('clausie_output.txt', 'r') as fp:
-		last_line = None
-		last_id = 0
-		for line in fp:
-			if line[0] == '#':
-				last_line = line
-			# print(line)
-			elif line.split()[0] == last_id:
-				continue
-			else:
-				last_id = line.split()[0]
-				# This is an output, last line has clause type
-				sep_line = last_line.split()
-				clause_type = sep_line[2]
-				# print(line.split())
-
-				verb_instance = None
-				for i, item in enumerate(sep_line[3:]):
-					if 'V:' in item:
-						verb_instance = sep_line[3 + i + 1]
-						break
-				verb_instance = verb_instance.split('@')[0]
-				verb_lemmas.append(verb_instance)
-				clause_types.append(clause_type)
-
-	cndt_fids = []
-	sent_zip = zip(clause_types, verb_lemmas)
-	for ctype, vlemma in sent_zip:
-		print('{} : {}'.format(ctype, vlemma))
-		frameids = CLAUSIE_FRAME_DICT[ctype]
-		for i, synset in enumerate(wn.synsets(vlemma)):
-			fids = synset.frame_ids()
-			interfids = list(set(frameids) & set(fids))
-			if len(interfids) > 0:
-				cndt_fids.extend(interfids)
-				print('{}: {}'.format(i, synset.definition()))
-		print('\n')
+	# returns Sentences
+	return read_clausie_output('clausie_output.txt')
 
 def read_clausie_output(text_file_name):
 	sents = []
@@ -94,13 +87,12 @@ def read_clausie_output(text_file_name):
 	with open(text_file_name, 'r') as clausie_text:
 
 		for line in clausie_text:
-			if line[:5] == '# Line':
+			if line[:6] == '# Line':
 				if not started:
 					started = True
-				if sent != []:
+				if sent:
 					sents.append(sent)
 					sent = []
-				sent.append(line)
 			if started:
 				sent.append(line)
 
@@ -108,5 +100,9 @@ def read_clausie_output(text_file_name):
 
 if __name__ == '__main__':
 
-	read_clausie_output('clausie_output.txt')
+	# test: give sample sentences, read them and get output
+	sample_text = [b'The body floats to the surface of the water.\n', b'He shoots first, asks questions later.\n']
+	sentences = clausie(text=sample_text)
 
+	for sent in sentences:
+		print(sent)
